584b8ff4f069589f9d8933355d311c91c30a8e8e
nhatnhh <hoangnhat3103@gmail.com>
2024-08-03 20:24:04 +0700

    example custom validate

--- src/user/user.validator.ts
+++ src/user/user.validator.ts
@@ -1,4 +1,6 @@
 import { Validator } from '../common/validation/base-validator';
+import { NumberValidator } from '../common/validation/number-validator';
+import { StringValidator } from '../common/validation/string-validator';
 import { RequestUserCreate } from './user.dto';
 import { User } from './user.schema';
 
@@ -7,8 +9,24 @@ export class UserValidator extends Validator<User, UserValidator> {
     super();
   }
   validate(payload: RequestUserCreate): UserValidator | Promise<UserValidator> {
-    const { name, age, email, description } = payload;
+    const { name, age, email } = payload;
 
+    // Validate name
+    const nameValidated = new StringValidator(name)
+      .required()
+      .isString()
+      .maxLength(150);
+
+    // Validate age
+    const ageValidated = new NumberValidator(age).required().max(120).min(0);
+
+    // Validate email
+    const emailValidated = new StringValidator(email)
+      .required()
+      .isString()
+      .isEmail()
+      .minLength(1)
+      .maxLength(300);
     return this;
   }
 }
